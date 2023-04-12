823f5d8829afe3dac128cc98ac90cbc879ad24fc
RJ Green <rjgreen97@gmail.com>
2023-04-12 11:11:52 -0600

    update processor

--- utils/preprocessor.py
+++ utils/preprocessor.py
@@ -1,14 +1,42 @@
 import json
 import re
+import os
+import glob
 
 
-def book_to_sentences(book):
+def merge_jsons(path="data/processed"):
+    files = glob.glob(os.path.join(path, "*.json"))
+    merged = []
+    for file in files:
+        with open(file, "r", encoding="utf-8") as f:
+            merged.extend(json.load(f))
+    with open("data/processed/all_books.json", "w") as f:
+        json.dump(merged, f, ensure_ascii=False)
+
+
+def preprocess_text(book):
     with open(book, "r") as book:
         raw_text = book.read()
         sentence_boundary_pattern = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
         sentences = re.split(sentence_boundary_pattern, raw_text)
         cleaned_sentences = clean_sentences(sentences)
-        sentences_to_json(cleaned_sentences)
+        book_name = get_book_name(book)
+        sentences_to_json(cleaned_sentences, book_name)
+
+
+def get_book_name(book):
+    book_name = str(book.name)
+    match = re.search(r"(?<=\/)(\w+)(?=\.)", book_name)
+    if match:
+        filename = match.group(1)
+        return filename
+
+
+def sentences_to_json(sentences, book_name):
+    with open(
+        f"data/processed/{book_name}_sentences.json", "w", encoding="utf-8"
+    ) as sentences_json:
+        json.dump(sentences, sentences_json, ensure_ascii=False)
 
 
 def clean_sentences(sentences):
@@ -17,12 +45,3 @@ def clean_sentences(sentences):
         sentences = [sentence.replace(char, "") for sentence in sentences]
     clean_sentences = [sentence for sentence in sentences if len(sentence) > 10]
     return clean_sentences
-
-
-def sentences_to_json(sentences):
-    with open("data/processed/sentences.json", "w", encoding="utf-8") as sentences_json:
-        json.dump(sentences, sentences_json, ensure_ascii=False)
-
-
-if __name__ == "__main__":
-    book_to_sentences("data/books/gameofthrones.txt")
