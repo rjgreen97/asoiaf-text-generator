6588624612c6cf3c2ee7a232241730f513985bf1
RJ Green <rjgreen97@gmail.com>
2023-04-12 14:51:03 -0600

    training session logic

--- src/training/training_session.py
+++ src/training/training_session.py
@@ -1,1 +1,76 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import datetime
+import os
+
+import torch
+from torch.utils.data import DataLoader
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    IntervalStrategy,
+    Trainer,
+    TrainingArguments,
+)
+
+from src.data.asoiaf_dataset_builder import ASOIAFDatasetBuilder
+from src.training.training_session_arg_parser import TrainingSessionArgParser
+
+
+class TrainingSession:
+    def __init__(self, args):
+        self.args = args
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            self.args.model_name,
+            bos_token="<|beginning_of_sentence|>",
+            eos_token="<|end_of_sentence|>",
+            pad_token="<|pad|>",
+        )
+
+    def run(self):
+        self.create_datasets()
+        self.create_dataloaders()
+        self.create_model()
+        self.create_trainer()
+        self.trainer.train()
+
+    def create_datasets(self):
+        dataset_builder = ASOIAFDatasetBuilder(
+            self.args.data_filepath, self.args.model_name, self.args.val_percent
+        )
+        self.train_dataset, self.val_dataset = dataset_builder.build()
+
+    def create_dataloaders(self):
+        self.train_dataloader = DataLoader(
+            self.train_dataset, batch_size=self.args.batch_size, shuffle=True
+        )
+        self.val_dataloader = DataLoader(
+            self.val_dataset, batch_size=self.args.batch_size
+        )
+
+    def create_model(self):
+        self.model = AutoModelForCausalLM.from_pretrained(self.args.model_name)
+        self.model.resize_token_embeddings(len(self.tokenizer))
+
+    def create_trainer(self):
+        time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        training_args = TrainingArguments(
+            output_dir=f"./models/{self.args.model_name}/{time}",
+            num_train_epochs=self.args.num_epochs,
+            per_device_train_batch_size=self.args.batch_size,
+        )
+        self.trainer = Trainer(
+            model=self.model,
+            args=training_args,
+            train_dataset=self.train_dataset,
+            eval_dataset=self.val_dataset,
+            data_collator=lambda data: {
+                "input_ids": torch.stack([f["input_ids"] for f in data]),
+                "attention_mask": torch.stack([f["attention_mask"] for f in data]),
+                "labels": torch.stack([f["input_ids"] for f in data]),
+            },
+        )
+
+
+if __name__ == "__main__":
+    args = TrainingSessionArgParser().parse_args()
+    session = TrainingSession(args)
+    session.run()
--- src/training/training_session_arg_parser.py
+++ src/training/training_session_arg_parser.py
@@ -3,11 +3,46 @@ from argparse import ArgumentParser
 
 class TrainingSessionArgParser:
     def __init__(self):
-        self.parser = ArgumentParser(description='Training session arguments')
+        self.parser = ArgumentParser(description="Training session arguments")
         self.add_args()
 
     def add_args(self):
-        pass
+        self.parser.add_argument(
+            "--data_filepath",
+            type=str,
+            default="data/processed/all_books.json",
+            help="Path to the json file containing text data",
+        )
+        self.parser.add_argument(
+            "--model_name",
+            type=str,
+            default="facebook/opt-125m",
+            help="HuggingFace CausalLM pre-trained model to be fine tuned",
+        )
+        self.parser.add_argument(
+            "--epochs",
+            type=int,
+            default=64,
+            help="Number of epochs to train the model",
+        )
+        self.parser.add_argument(
+            "--batch_size",
+            type=int,
+            default=32,
+            help="Batch size to be used during training",
+        )
+        self.parser.add_argument(
+            "--learning_rate",
+            type=float,
+            default=1e-4,
+            help="Learning rate to be used during training",
+        )
+        self.parser.add_argument(
+            "--val_percent",
+            type=float,
+            default=0.2,
+            help="Percentage of the dataset to be used for validation",
+        )
 
     def parse_args(self):
         return self.parser.parse_args()
