9c37ee05631a48377c029a9ee0494ecbc3b92bf1
RJ Green <rjgreen97@gmail.com>
2023-04-13 15:00:31 -0500

    reorg arg structure

--- src/generate/text_generator.py
+++ src/generate/text_generator.py
@@ -3,7 +3,7 @@ import argparse
 
 
 class TextGenerator:
-    def __init__(self, model_name, model_path, max_length=300):
+    def __init__(self, model_name, model_path, max_length=1000):
         self.max_length = max_length
         self.model_name = model_name
         self.bos_token = "<|beginning_of_sentence|>"
@@ -17,7 +17,7 @@ class TextGenerator:
         self.model = AutoModelForCausalLM.from_pretrained(self.model_path)
         self.model.eval()
 
-    def generate(self, prompt):
+    def generate(self, prompt, num_return_sequences=3):
         prompt = self.bos_token + prompt
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
 
@@ -27,15 +27,15 @@ class TextGenerator:
             bos_token_id=self.tokenizer.bos_token_id,
             eos_token_id=self.tokenizer.eos_token_id,
             pad_token_id=self.tokenizer.pad_token_id,
+            num_return_sequences=num_return_sequences,
             do_sample=True,
             top_k=50,
             top_p=0.95,
             temperature=0.9,
-            num_return_sequences=3,
         )
 
         for i, output in enumerate(generated_text_output):
-            print(f"{i}: {self.tokenizer.decode(output, skip_special_tokens=True)}")
+            print(f"{i+1}: {self.tokenizer.decode(output, skip_special_tokens=True)}")
 
     def generate_loop(self):
         while True:
